ada7e35606ad8d744b5a87b51ac87da7508912ec
John Durrant <JohnD-EE@users.noreply.github.com>
2017-07-11 08:13:01 +0100

    Update mappings.sol

--- CodeResources/Solidity/LanguageReference/mappings.sol
+++ CodeResources/Solidity/LanguageReference/mappings.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.4.0;
 
-contract MappingsExample {
+contract MappingsExamples {
 
+    //Use a mapping to store balances against addresses
+    mapping(address => uint) public balances;
 
+    function addBalance(uint _value) {
+        balances[msg.sender] = _value;
+    }
 
-
-
-
-
-
-
+    function getBalance(address _address) constant returns (uint) {
+        return balances[_address];
+    }
 
 }
