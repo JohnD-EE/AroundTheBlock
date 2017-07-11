4d0d17559076eb1d2418efe3ace80e03176ae541
John Durrant <JohnD-EE@users.noreply.github.com>
2017-07-11 10:51:52 +0100

    Update mappingSimple.sol

--- CodeResources/Solidity/LanguageReference/mappingSimple.sol
+++ CodeResources/Solidity/LanguageReference/mappingSimple.sol
@@ -7,12 +7,15 @@ contract MappingSimple {
 
     /*A setter taking the _value argument to store in the balances mapping at
     the address index*/
+    /// @param _value The amount sent from the sender's address
     function addBalance(uint _value) {
         balances[msg.sender] = _value;
     }
 
     /*There is no need for this getter beccause balances mapping is public,
     this shows how value could be retreived from the mapping*/
+    /// @param _address The address stored in the mapping 
+    /// @return The balance stored in the mapping for the given address
     function getBalance(address _address) constant returns (uint) {
         
         /*returns the value found at the _address index, as if it was an array
